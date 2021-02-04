5d411912449711f5e65a5b6fad4a2c0d650114dd
xichen1 <xichen.pan@gmail.com>
2021-02-03 18:44:37 -0700

    fix: move post of book detail to backend

--- src/services/books.js
+++ src/services/books.js
@@ -1,33 +1,20 @@
 import axios from 'axios';
 const baseUrl = '/api/books';
 
-const getAll = () => {
-  const request = axios.get(baseUrl);
-  return request.then(response => {
-    return response.data;
-  });
+const getAll = async () => {
+  const response = await axios.get(baseUrl);
+  return response.data;
 }
 
 const create = async newObject => {
   const response = await axios.post(baseUrl, newObject);
-
   const data = response.data;
-
-  await axios.post('/api/bookdetails', {
-    title: data.title,
-    author: data.author,
-    about: data.about,
-    book: data.id
-  })
-
   return data;
 }
 
-const update = (id, newObject) => {
-  const requset = axios.put(`${baseUrl}/${id}`, newObject)
-  return requset.then(response => {
-    return response.data;
-  });
+const update = async (id, newObject) => {
+  const response = await axios.put(`${baseUrl}/${id}`, newObject);
+  return response.data;
 }
 
 export const bookServices = {
